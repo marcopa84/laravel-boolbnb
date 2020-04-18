59e4d41b001a4f5aaf76d06d65130b7b826da1b8
Marco <marcopa84@hotmail.it>
2020-04-18 17:13:45 +0200

    Registered Apartments index OK

--- resources/views/registered/apartments/index.blade.php
+++ resources/views/registered/apartments/index.blade.php
@@ -2,34 +2,47 @@
 
 @section('main')
 <div class="container my-5">
-<ul>
-   @foreach ($apartments as $apartment)
-   <li>{{$apartment->title}}
-      <ul>
-         <li>{{$apartment->description}}</li>
-         <li>{{$apartment->beds_number}}</li>
-         <li>{{$apartment->rooms_number}}</li>
-         <li>{{$apartment->bathrooms_number}}</li>
-         <li>{{$apartment->price}}</li>
-         <li>{{$apartment->size}}</li>
-         <li>{{$apartment->address}}</li>
-         <li>{{$apartment->latitude}}</li>
-         <li>{{$apartment->longitude}}</li>
-         <li>{{$apartment->featured_image}}</li>
-         <li><a href="{{route('registered.apartments.show', $apartment)}}">Show</a></li>
-         <li><a href="{{route('registered.apartments.edit', $apartment)}}">EDIT</a></li>
-         <li>
-            <form action="{{route('registered.apartments.destroy', $apartment)}}" method="post">
-              @csrf
-              @method('DELETE')
-              <button class="btn btn-danger" type="submit">Delete</button>  
-            </form>
-         
-         </li>
-      </ul>
-   </li>
-   @endforeach
-</ul>
+   <div class="row">
+      <div class="col">
+         <div class="add-apartment mb-4 d-flex justify-content-end w-100">
+            <a href="{{route('registered.apartments.create')}}"><span>Aggiungi un appartamento</span><button class="btn btn-outline-dark ml-2"><i class="fas fa-plus"></i></button></a>
+         </div>
+      </div>
 
+   </div>
+   <div class="row">
+      <div class="col-12">
+         @if (empty($apartments))
+            <div class="alert alert-info" role="alert">
+               <p>Non hai appartamenti registrati!</p>
+            </div>
+         @else
+            @foreach ($apartments as $apartment)
+            <div class="card dash">
+               <div class="card-image">
+                  <img class="card-img-top" src="{{asset($apartment->featured_image)}}" alt="Immagine di anteprima dell'appartamento">
+               </div>
+               <div class="card-body">
+                  <div class="card-body-text">
+                     <h3 class="card-title">{{$apartment->title}}</h3>
+                     <p class="card-text lead">{{$apartment->address}}</p>
+                  </div>
+                  <div class="card-dash-buttons">
+                     <a href="{{route('registered.apartments.show', $apartment)}}"><button class="btn btn-dark">Visualizza</button></a>
+                     <a href="{{route('registered.apartments.edit', $apartment)}}"><button class="btn btn-dark">Modifica</button></a>
+                     <a href="#"><button class="btn btn-dark">Statistiche</button></a>
+                     <form class="d-inline" action="{{route('registered.apartments.destroy', $apartment)}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Cancella</button>
+                     </form>
+                  </div>
+               </div>
+            </div>
+            @endforeach
+         @endif
+      </div>            
+   </div>
+</div>
 
 @endsection
