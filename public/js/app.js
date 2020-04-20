f326fe0851703443840a2c283e91732743fcf868
Danilo <danilo.campagnolo.93@gmail.com>
2020-04-20 10:58:18 +0200

    features fixed

--- public/js/app.js
+++ public/js/app.js
@@ -42569,8 +42569,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! F:\Alessandro\Boolean.careers\Esercitazioni\mamp_public\laravel-boolbnb\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! F:\Alessandro\Boolean.careers\Esercitazioni\mamp_public\laravel-boolbnb\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/danilo/Desktop/mamp_public/laravel-boolbnb/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/danilo/Desktop/mamp_public/laravel-boolbnb/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/registered/apartments/show.blade.php
+++ resources/views/registered/apartments/show.blade.php
@@ -1,57 +1,73 @@
 @extends('layouts.layout')
 
-<!-- {{-- <ul>
-   <li>{{$apartment->title}}
-      <ul>
-         <li>{{$apartment->description}}</li>
-         <li>{{$apartment->beds_number}}</li>
-         <li>{{$apartment->rooms_number}}</li>
-         <li>{{$apartment->bathrooms_number}}</li>
-         <li>{{$apartment->price}}</li>
-         <li>{{$apartment->size}}</li>
-         <li>{{$apartment->address}}</li>
-         <li>{{$apartment->latitude}}</li>
-         <li>{{$apartment->longitude}}</li>
-         <li><img src="{{asset($apartment->featured_image)}}" alt="{{$apartment->title}}" class="img-thumbnail"></li>
-         <li>{{$apartment->visible}}</li>
-      </ul>
-   </li>
-</ul> --}} -->
-
 @section('main')
 <div class="container layout-apartment my-5">
    <div class="main-content">
       <h1>{{$apartment->title}}</h1>
       <img src="{{asset($apartment->featured_image)}}" title="Immagine in evidenza">
       <h4 class="apartment-desc-title">Descrizione</h4>
-      <p class="apartment-desc-text lead">{{$apartment->description}}</p>
+      <p class="apartment-desc-text lead">{{$apartment->description}}
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum unde, quo saepe dicta quisquam illo excepturi molestias non voluptatum id minima aut dolor repellendus nam. Repellendus, reiciendis beatae. Harum? adipisicing elit. Sint harum unde, quo saepe dicta quisquam illo excepturi molestias non voluptatum id minima aut dolor repellendus nam. Repellendus, reiciendis beatae. Harum? adipisicing elit. Sint harum unde, quo saepe dicta quisquam illo excepturi molestias non voluptatum id minima aut dolor repellendus nam. Repellendus, reiciendis beatae. Harum? adipisicing elit. Sint harum unde, quo saepe dicta quisquam illo excepturi molestias non voluptatum id minima aut dolor repellendus nam. Repellendus, reiciendis beatae. Harum? adipisicing elit. Sint harum unde, quo saepe dicta quisquam illo excepturi molestias non voluptatum id minima aut dolor repellendus nam. Repellendus, reiciendis beatae. Harum? adipisicing elit. Sint harum unde, quo saepe dicta quisquam illo excepturi molestias non voluptatum id minima aut dolor repellendus nam. Repellendus, reiciendis beatae. Harum?
+      </p>
 
       <div class="apartment-features">
          <h4 class="apartment-features-title">Servizi</h4>
          <div class="apartment-features-list">
-            @if ($apartment->features->has(0))
-               <span><i class="fas fa-wifi"></i> WiFi</span>
+            @forelse ($apartment->features as $feature)
+            @if ($feature->id == 1)
+            <span><i class="fas fa-wifi"></i> Wi-Fi</span>
             @endif
-            @if ($apartment->features->has(1))
+            @if ($feature->id == 2)
                <span><i class="fas fa-parking fa-1.5x"></i> Posto auto</span>
             @endif
-            @if ($apartment->features->has(2))
+            @if ($feature->id == 3)
                <span><i class="fas fa-swimmer"></i> Piscina</span>
             @endif
-            @if ($apartment->features->has(3))
+            @if ($feature->id == 4)
                <span><i class="fas fa-concierge-bell"></i> Portineria</span>
             @endif
-            @if ($apartment->features->has(4))
+            @if ($feature->id == 5)
                <span><i class="fas fa-hot-tub"></i> Sauna</span>
             @endif
-            @if ($apartment->features->has(5))
+            @if ($feature->id == 6)
                <span><i class="fas fa-water"></i> Vista mare</span>
             @endif
+            @empty
+                Nessun servizio aggiuntivo offerto.
+            @endforelse
+            
          </div>
       </div>
    </div> <!-- / main-content -->
    <div class="sidebar">
       Sidebar
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia neque, sint dicta exercitationem accusamus est, quisquam ab, autem sit nobis assumenda aliquid consectetur et doloremque molestias nostrum tempora tempore amet.lorem
+      Sidebar
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia neque, sint dicta exercitationem accusamus est, quisquam ab, autem sit nobis assumenda aliquid consectetur et doloremque molestias nostrum tempora tempore amet.lorem
+      Sidebar
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia neque, sint dicta exercitationem accusamus est, quisquam ab, autem sit nobis assumenda aliquid consectetur et doloremque molestias nostrum tempora tempore amet.lorem
+      Sidebar
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia neque, sint dicta exercitationem accusamus est, quisquam ab, autem sit nobis assumenda aliquid consectetur et doloremque molestias nostrum tempora tempore amet.lorem
+      Sidebar
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia neque, sint dicta exercitationem accusamus est, quisquam ab, autem sit nobis assumenda aliquid consectetur et doloremque molestias nostrum tempora tempore amet.lorem
+      Sidebar
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia neque, sint dicta exercitationem accusamus est, quisquam ab, autem sit nobis assumenda aliquid consectetur et doloremque molestias nostrum tempora tempore amet.lorem
+      Sidebar
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia neque, sint dicta exercitationem accusamus est, quisquam ab, autem sit nobis assumenda aliquid consectetur et doloremque molestias nostrum tempora tempore amet.lorem
+      Sidebar
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia neque, sint dicta exercitationem accusamus est, quisquam ab, autem sit nobis assumenda aliquid consectetur et doloremque molestias nostrum tempora tempore amet.lorem
+      Sidebar
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia neque, sint dicta exercitationem accusamus est, quisquam ab, autem sit nobis assumenda aliquid consectetur et doloremque molestias nostrum tempora tempore amet.lorem
+      Sidebar
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia neque, sint dicta exercitationem accusamus est, quisquam ab, autem sit nobis assumenda aliquid consectetur et doloremque molestias nostrum tempora tempore amet.lorem
+      Sidebar
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia neque, sint dicta exercitationem accusamus est, quisquam ab, autem sit nobis assumenda aliquid consectetur et doloremque molestias nostrum tempora tempore amet.lorem
+      Sidebar
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia neque, sint dicta exercitationem accusamus est, quisquam ab, autem sit nobis assumenda aliquid consectetur et doloremque molestias nostrum tempora tempore amet.lorem
+      Sidebar
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia neque, sint dicta exercitationem accusamus est, quisquam ab, autem sit nobis assumenda aliquid consectetur et doloremque molestias nostrum tempora tempore amet.lorem
+      Sidebar
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia neque, sint dicta exercitationem accusamus est, quisquam ab, autem sit nobis assumenda aliquid consectetur et doloremque molestias nostrum tempora tempore amet.lorem
    </div> <!-- / sidebar -->
 </div>
 
