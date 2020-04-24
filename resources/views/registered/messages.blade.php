7975d6be286bc5ff5250f0f94435a07775d94bc8
Marco <marcopa84@hotmail.it>
2020-04-24 17:32:31 +0200

    Messages OK!

--- resources/views/registered/messages.blade.php
+++ resources/views/registered/messages.blade.php
@@ -1,12 +1,25 @@
 @extends('layouts.layout')
 
 @section('main')
+<div class="container my-5">
+    @if (!count($messages))
+    <div class="alert alert-info" role="alert">
+        <p>Non hai ricevuto ancora messaggi.</p>
+    </div>
+    @else
+    <ul>
     @foreach ($messages as $message)
+    <li>MESSAGGIO:
         <ul>
             <li> {{$message->email}} </li>
             <li> {{$message->content}} </li>
             <li> {{$message->created_at}} </li>
         </ul>
-        
+    </li>      
     @endforeach
+    </ul>
+    @endif
+</div>
+
+
 @endsection
