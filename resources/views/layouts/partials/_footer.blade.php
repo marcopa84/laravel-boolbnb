50e1da3916c80cd1737d62bd8602a0be4d707ff5
Lorenzo <lorenzo.franzone@gmail.com>
2020-05-03 16:56:30 +0200

    footer flex ok

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -1,8 +1,12 @@
-   <footer>
-     @yield('footer')
-   </footer>
-   @yield('styles')
-   @yield('scripts')
+  <footer>
+    
+    <p>Copyright &copy; 2020 - BoolbnbTeam7 srl | P.IVA: 01234567890</p>
+    
+    @yield('footer')
+  
+  </footer>
+  @yield('styles')
+  @yield('scripts')
 
 </body>
 </html>
